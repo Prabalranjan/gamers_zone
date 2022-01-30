c0c3dee06ea46e18b4eabb045e8bb721aa82f8be
Prabal Ranjan Goswami <71269601+Prabalranjan@users.noreply.github.com>
2022-01-31 04:21:03 +0530

    final with payment.
    final upload DB is razorpay

---
+++ viewall.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View all</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="viewallCSS.css">
+</head>
+<body>
+    <div class="container mx-auto mt-4">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/CODWW2-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Call Of Duty WW 2</h5>
+              <h6 class="card-subtitle mb-2 text-muted">FPS</h6>
+          <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+             <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+          <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+             <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/far cry piramal-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Far Cry Primal</h5>
+              <h6 class="card-subtitle mb-2 text-muted">FPS adventure</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+             <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+          <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+                <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/crysis-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Crysis</h5>
+              <h6 class="card-subtitle mb-2 text-muted">FPS adventure</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+        </div>
+          
+          <div class="col-md-4">
+            <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/avengers-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title"> Marvel's Avengers</h5>
+              <h6 class="card-subtitle mb-2 text-muted">TPS mission</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+             <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/the crew-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">The Crew</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Online Racing</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+                <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/dark soul-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Dark Soul</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Horror, adventure</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+        </div>
+          
+          <div class="col-md-4">
+            <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/for honor-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">For Honour</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Online Tp battle, mission</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+             <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/arkham city-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Arkham City</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Batman adventure, Detective</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+                <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/ninja blade-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Ninja Blade</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Tp adventure mission</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+        </div>
+          
+          <div class="col-md-4">
+            <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/splinter cells-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Splinter Cells</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Stealth missions</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+             <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/bloodborne-rezise.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Bloodborne</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Blood, mission</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+          </div>    
+                <div class="col-md-4">
+      <div class="card" style="width: 20rem;">
+        <img src="assets/img/gallery/darksiders-resize.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Darksiders II</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Horror, adventure, combat</h6>
+              <p class="card-text">FPS game series by activation , with great story line and RTX enable graphis presentss you Call Of Duty World war 2.</p>
+              <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Add to cart</a>
+           <a href="product desc.php" class="btn "><i class="fab fa-github"></i> Buy now</a>
+        </div>
+        </div>
+        </div>
+      </div>
+        </div>
+</body>
+</html>
